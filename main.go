69ed82838a3aee9e720743e8bdd9b22f1ab2dbb8
chenbochong <isesame_live@163.com>
2020-09-16 18:46:15 +0800

    测试

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
- 
-import (
-	"fmt"
-	"os"
-	"strings"
-)
- 
-func main() {
-	aSecret := os.Getenv("A_SECRET")
-	fmt.Println(aSecret)
-	fmt.Println(reverseString(aSecret))
-}
- 
-func reverseString(s string) string {
-	r := []rune(s)
-	sb := strings.Builder{}
-	for i := len(r) - 1; i >= 0; i-- {
-		sb.WriteRune(r[i])
-	}
-	return sb.String()
+
+import "fmt"
+
+func main(){  
+    fmt.Printf("Hello World!");
 }
